8a4d9a04dc6840f26946c9c18654a41a42a56b48
vinoth <viinoth.p@gmail.com>
2022-06-11 11:14:44 +0530

    Added the grid.f90 file with all the 2D single block features

--- grid.f90
+++ grid.f90
@@ -10,6 +10,18 @@ module grid
     subroutine meshreader
       implicit none
 
+      integer(i8) :: nb
+      integer(i32) :: nxmax, nymax, nxdomain, nydomain, nzdomain
+      integer(i32) :: i, j, imxnb, jmxnb
+
+      logical :: file_status
+      real(real64) :: x1, x2, x3, x4, y1, y2, y3, y4
+      real(real64) :: dl1, dl2, dl3, dl4
+
+      nxmax = 0
+      nymax = 0
+      nblocks = 0
+
       inquire(file=gridfile, exist=file_status)
 
       if (file_status) then
@@ -30,6 +42,10 @@ module grid
             write(error_unit, 'a') 'Aborting the program'
       end if
 
+      close(13)
+      ! Allocation of variables
+      call mallocate_variables(nxmax, nymax, nblocks)
+
       ! Read the grid file to get the values
       do nb = 1, nblocks
          print *, 'Reading the Block ', dummy(8:)
@@ -48,10 +64,75 @@ module grid
          end do
       end do
 
-      close (13)
+      do nb = 1, nblocks
+         imxnb = imax(nb)
+         do j = 1, jmax(nb)
+            ! left boundary cells
+            x(0, j, nb) = two*x(1, j, nb) - x(2, j, nb)
+            x(-1, j, nb) = two*x(0, j, nb) - x(1, j, nb)
+            x(-2, j, nb) = two*x(-1, j, nb) - x(0, j, nb)
 
-      call mallocate_variables(nxmax, nymax, nblocks)
+            y(0, j, nb) = two*y(1, j, nb) - y(2, j, nb)
+            y(-1, j, nb) = two*y(0, j, nb) - y(1, j, nb)
+            y(-2, j, nb) = two*y(-1, j, nb) - y(0, j, nb)
 
+            ! right boundary cells
+            x(imxnb+1, j, nb) = two*x(imxnb, j, nb) - x(imxnb-1, j, nb)
+            x(imxnb+2, j, nb) = two*x(imxnb+1, j, nb) - x(imxnb1, j, nb)
+            x(imxnb+3, j, nb) = two*x(imxnb+2, j, nb) - x(imxnb+1, j, nb)
+
+            y(imx+nb+1, j, nb) = two*y(imxnb, j, nb) - y(imxnb-1, j, nb)
+            y(imxnb+2, j, nb) = two*y(imxnb+1, j, nb) - y(imxnb1, j, nb)
+            y(imxnb+3, j, nb) = two*y(imxnb+2, j, nb) - y(imxnb+1, j, nb)
+        end do
+
+        jmxnb = jmax(nb)
+        do i = 1, imax(nb)
+           x(i, 0, nb) = two*x(i, 1, nb) - x(i, 2, nb)
+           x(i, -1, nb) = two*x(i, 0, nb) - x(i, 1, nb)
+           x(i, -2, nb) = two*x(i, -1, nb) - x(i, 0, nb)
+
+           y(i, 0, nb) = two*y(i, 1, nb) - y(i, 2, nb)
+           y(i, -1, nb) = two*y(i, 0, nb) - y(i, 1, nb)
+           y(i, -2, nb) = two*y(i, -1, nb) - y(i, 0, nb)
+
+           x(i, jmxnb+1, nb) = two*x(i, jmxnb, nb) - x(i, jmxnb-1, nb)
+           x(i, jmxnb+2, nb) = two*x(i, jmxnb+1, nb) - x(i, jmxnb, nb)
+           x(i, jmxnb+3, nb) = two*x(i, jmxnb+2, nb) - x(i, jmxnb+1, nb)
+
+           y(i, jmxnb+1, nb) = two*y(i, jmxnb, nb) - y(i, jmxnb-1, nb)
+           y(i, jmxnb+2, nb) = two*y(i, jmxnb+1, nb) - y(i, jmxnb, nb)
+           y(i, jmxnb+3, nb) = two*y(i, jmxnb+2, nb) - y(i, jmxnb+1, nb)
+        end do
+      end do
+
+      do nb = 1, nblocks
+         do j = 1, ny(nb)
+            do i = 1, nx(nb)
+               x1 = x(i, j, nb)
+               x2 = x(i+1, j, nb)
+               x3 = x(i+1, j+1, nb)
+               x4 = x(i, j+1, nb)
+
+               y1 = y(i, j, nb)
+               y2 = y(i+1, j, nb)
+               y3 = y(i+1, j+1, nb)
+               y4 = y(i, j+1, nb)
+
+               xc(i, j, nb) = quarter*(x1 + x2 + x3 + x4)
+               yc(i, j, nb) = quarter*(y1 + y2 + y3 + y4)
+
+               dl1 = dsqrt((x2 - x1)**2 + (y2 - y1)**2)
+               dl2 = dsqrt((x3 - x2)**2 + (y3 - y2)**2)
+               dl3 = dsqrt((x4 - x3)**2 + (y4 - y3)**2)
+               dl4 = dsqrt((x1 - x4)**2 + (y1 - y4)**2)
+
+               dl(i, j, nb) = dmin1(dl1, dl2, dl3, dl4)
+               a(i, j, nb) = half*((x3 - x1)*(y4 - y2) - &
+                    (x4 - x2)*(y3 - y1))
+            end do
+         end do
+      end do
     end subroutine meshreader
 
 
